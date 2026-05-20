2bdff166c28199ba064276263c2d3a8870dd856c
Sadig Imanov <sadiqimanov69@gmail.com>
2026-05-20 04:25:52 +0400

    authDomain deyisdi

--- js/firebase.js
+++ js/firebase.js
@@ -7,7 +7,7 @@ import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove }
 
 const firebaseConfig = {
   apiKey: "AIzaSyCBUBycbWSIC2DHx-e8EuiQQYneYp-mfD0",
-  authDomain: "akkordlarim-35c64.firebaseapp.com",
+  authDomain: "sadigimanov.github.io",
   projectId: "akkordlarim-35c64",
   storageBucket: "akkordlarim-35c64.firebasestorage.app",
   messagingSenderId: "406624151690",
